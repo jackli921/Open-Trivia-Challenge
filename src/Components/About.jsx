4bccf94a9b2f5c5ae47bb37d5da1ba289ad34c77
Jack <li.jack0707@gmail.com>
2023-03-24 16:19:16 -0400

    style: changed text color styling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,18 +2,33 @@ import React, { PureComponent } from 'react'
 
 export default function About(){
     return (
-        <div className='about'>
-            <h3>About</h3>
-            <p>This app was created by {<a href="https://www.linkedin.com/in/jackli921/">Jack</a>} as a solo project of 
-            {<strong> Module 11 - React Basics </strong>}of the <a href="https://scrimba.com/learn/frontend">Scrimba Frontend Developer Course</a>
-            </p>
-        
-            <p>The features of this app was inspired by Daniel Beck Rose, who is also a Scrimba student; You
-                can check <a href="https://daniels-quiz-o-matic.netlify.app/">his version</a> </p>
-        
-            <p>All questions are from the {<a herf="https://opentdb.com/api_config.php">Open Trivia API</a>}</p>
-        
-            <p>Jack has also built a dating app for dogs called {<a href="https://tiny-mochi-3e1c9c.netlify.app/">Tindog</a>} If you want to get in touch with Jack, just <a href="mailto: li.jack0707@gmail.com">Email</a></p>
-        </div>
-    )
+      <div className="about">
+        <h3>About</h3>
+        <p>
+          This app was created by{" "}
+          {<a href="https://www.linkedin.com/in/jackli921/">Jack</a>} as a solo
+          project of
+          {<strong> Module 11 - React Basics </strong>}of the{" "}
+          <a href="https://scrimba.com/learn/frontend">
+            Scrimba Frontend Developer Course
+          </a>
+        </p>
+
+        <p>
+          The features of this app was inspired by Daniel Beck Rose, who is also
+          a Scrimba student; You can check{" "}
+          <a href="https://daniels-quiz-o-matic.netlify.app/">his version</a>{" "}
+        </p>
+
+        <p>
+          All questions are from the{" "}
+          {<a href="https://opentdb.com/api_config.php">Open Trivia API</a>}
+        </p>
+
+        <p>
+          If you want to get in touch with Jack or see his other works, check
+          out his {<a href="https://jack-codes.netlify.app/">portfolio</a>}
+        </p>
+      </div>
+    );
 }
--- src/Components/Rules.jsx
+++ src/Components/Rules.jsx
@@ -4,9 +4,9 @@ export default function Rules(){
     return (
         <div className="rules">
             <ul >
-                <li>+1, +2, or +3 pts for every easy, medium, or hard questions answered <span className="green">correctly</span>! </li>
-                <li>-3, -2, or -1 pts for every easy, medium, or hard questions answered <span className="red">incorrectly</span>!</li>
-                <li> <span className='yellow'>Double your points</span> for each correct answer before the bonus timer runs out </li>
+                <li>+1, +2, or +3 pts for every easy, medium, or hard questions answered <span className="green-text">correctly</span>! </li>
+                <li>-3, -2, or -1 pts for every easy, medium, or hard questions answered <span className="red-text">incorrectly</span>!</li>
+                <li> <span className='yellow-text'>Double your points</span> for each correct answer before the bonus timer runs out </li>
                 <li>You get <strong>5 seconds</strong> of bonus time and <strong>7 seconds</strong> of penalty time per question</li>
                 <li>You lose points if you fail to answer all questions before the penalty timer runs out. E.g., 5 questions = -5 pts</li>
                 <li>You lose 2 pts for every 10s of overtime</li>
